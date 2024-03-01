e660647c898124b9bca110c53df4f69e938436b5
rohiitbagal <146849647+rohiitbagal@users.noreply.github.com>
2024-03-01 16:26:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 //routes
 app.use("*", function (req, res) {
