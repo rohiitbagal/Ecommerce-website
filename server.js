06e2040e583ad8fe72e14de301bea03d5d8f4177
rohiitbagal <146849647+rohiitbagal@users.noreply.github.com>
2024-03-03 10:52:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 app.use(express.static(path.join(__dirname, '../client')));
 
 //routes
