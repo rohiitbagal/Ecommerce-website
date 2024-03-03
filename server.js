e2f4438320ab6606b962816f8f5b8fe338856b65
rohiitbagal <146849647+rohiitbagal@users.noreply.github.com>
2024-03-03 09:59:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 //routes
 app.get('*', async (req,res) => {
